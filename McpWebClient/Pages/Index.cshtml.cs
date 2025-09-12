6ee4d4e49347df1654d4e2f2a72fb48a0e53d5e0
damienbod <damien_bod@hotmail.com>
2025-09-12 21:21:23 +0200

    Update access token

--- McpWebClient/Pages/Index.cshtml.cs
+++ McpWebClient/Pages/Index.cshtml.cs
@@ -1,19 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Web;
 
 namespace McpWebClient.Pages;
 
+[AuthorizeForScopes(ScopeKeySection = "api://96b0f495-3b65-4c8f-a0c6-c3767c3365ed/access_as_user")]
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly ITokenAcquisition _tokenAcquisition;
+    private readonly IConfiguration _configuration;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger,
+        IHttpClientFactory clientFactory,
+        ITokenAcquisition tokenAcquisition,
+        IConfiguration configuration)
     {
+        _clientFactory = clientFactory;
+        _tokenAcquisition = tokenAcquisition;
+        _configuration = configuration;
         _logger = logger;
     }
 
-    public void OnGet()
+    public async Task OnGetAsync()
     {
+        var accessToken = await _tokenAcquisition
+            .GetAccessTokenForUserAsync(["api://96b0f495-3b65-4c8f-a0c6-c3767c3365ed/access_as_user"]);
 
+        var t = "";
     }
 }
