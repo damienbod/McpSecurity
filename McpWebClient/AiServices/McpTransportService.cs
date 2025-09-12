5396a0f43bd2c30e1dad4e47e31d20878dc42edc
damienbod <damien_bod@hotmail.com>
2025-09-12 22:54:02 +0200

    Update web client

---
+++ McpWebClient/AiServices/McpTransportService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using ModelContextProtocol.Client;
+
+namespace McpWebClient;
+
+public class McpTransportService
+{
+    private readonly IDistributedCache _cache;
+
+    public McpTransportService(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public void InitialCacheIfNotExisting()
+    {
+        
+    }
+
+    public static IClientTransport CreateMcpTransport(HttpClient httpClient)
+    {
+        //var serverUrl = "https://localhost:7133/mcp";
+        var serverUrl = "https://mcpoauthsecurity-hag0drckepathyb6.westeurope-01.azurewebsites.net/mcp";
+        var transport = new SseClientTransport(new()
+        {
+            Endpoint = new Uri(serverUrl),
+            Name = "Secure Weather Client",
+            OAuth = new()
+            {
+                ClientName = "ProtectedMcpClient",
+                RedirectUri = new Uri("http://localhost:1179/callback"), 
+
+            }
+        }, httpClient);
+
+        return transport;
+    }
+}
