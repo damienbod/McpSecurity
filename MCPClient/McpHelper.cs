eab8f1230948b307ee8cafbbee14b73f60cde44b
damienbod <damien_bod@hotmail.com>
2025-09-12 16:34:54 +0200

    Use deployed server

--- MCPClient/McpHelper.cs
+++ MCPClient/McpHelper.cs
@@ -6,7 +6,8 @@ public class McpHelper
 {
     public static IClientTransport CreateMcpTransport(HttpClient httpClient)
     {
-        var serverUrl = "https://localhost:7133/mcp";
+        //var serverUrl = "https://localhost:7133/mcp";
+        var serverUrl = "https://mcpoauthsecurity-hag0drckepathyb6.westeurope-01.azurewebsites.net/mcp";
         var transport = new SseClientTransport(new()
         {
             Endpoint = new Uri(serverUrl),
