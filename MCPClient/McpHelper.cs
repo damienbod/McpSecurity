b37aea0ebcb249cd77856dd2d85d5b05c0f95581
damienbod <damien_bod@hotmail.com>
2025-09-13 08:59:12 +0200

    Update McpClient, no security

--- MCPClient/McpHelper.cs
+++ MCPClient/McpHelper.cs
@@ -6,18 +6,18 @@ public class McpHelper
 {
     public static IClientTransport CreateMcpTransport(HttpClient httpClient)
     {
-        //var serverUrl = "https://localhost:7133/mcp";
-        var serverUrl = "https://mcpoauthsecurity-hag0drckepathyb6.westeurope-01.azurewebsites.net/mcp";
+        var serverUrl = "https://localhost:7133/mcp";
+        //var serverUrl = "https://mcpoauthsecurity-hag0drckepathyb6.westeurope-01.azurewebsites.net/mcp";
         var transport = new SseClientTransport(new()
         {
             Endpoint = new Uri(serverUrl),
             Name = "Secure Weather Client",
-            OAuth = new()
-            {
-                ClientName = "ProtectedMcpClient",
-                RedirectUri = new Uri("http://localhost:1179/callback"),
+            //OAuth = new()
+            //{
+            //    ClientName = "ProtectedMcpClient",
+            //    RedirectUri = new Uri("http://localhost:1179/callback"),
 
-            }
+            //}
         }, httpClient);
 
         return transport;
