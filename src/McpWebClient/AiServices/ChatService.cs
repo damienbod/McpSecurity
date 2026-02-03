4f37b751e8b3ce9df25aa9cd000f7b38e8871c52
Raffael Nussbaumer <raffael.nussbaumer@isolutions.ch>
2026-02-03 08:42:09 +0100

    Configure function calling mode

--- src/McpWebClient/AiServices/ChatService.cs
+++ src/McpWebClient/AiServices/ChatService.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Headers;
 using ClientLibrary;
 using McpWebClient.AiServices.Elicitation;
@@ -35,7 +36,7 @@ public class ChatService
     private readonly IConfiguration _configuration;
     private readonly ElicitationCoordinator _elicitationCoordinator;
     private IChatClient _chatClient;
-    private IList<AIFunction> _mcpTools = [];
+    private IList<AIFunction> _tools = [];
     private IMcpClient _mcpClient = null!;
     private bool _initialized;
     private ApprovalMode _approvalMode = ApprovalMode.Auto;
@@ -77,19 +78,25 @@ public class ChatService
     {
         if (_initialized) return;
 
-        var accessToken = await _tokenAcquisition
-            .GetAccessTokenForUserAsync([_configuration["McpScope"]!]);
 
-        _mcpClient = await McpClientFactory.CreateAsync(CreateMcpTransport(clientFactory, accessToken), GetMcpOptions());
-        _mcpTools = await _mcpClient.GetMcpToolsAsAIFunctionsAsync();
+
+        if (_functionCallingMode == FunctionCallingMode.Local)
+        {
+            _tools = GetLocalTools();
+        }
+        else
+        {
+            _mcpClient = await McpClientFactory.CreateAsync(await CreateMcpTransport(clientFactory), GetMcpOptions());
+            _tools = await _mcpClient.GetMcpToolsAsAIFunctionsAsync();
+        }
 
         // Wrap chat client with function invocation if using elicitation or auto mode (auto-invoke)
-        if (_approvalMode == ApprovalMode.Elicitation || _approvalMode == ApprovalMode.Auto)
+        if (_approvalMode is ApprovalMode.Elicitation or ApprovalMode.Auto)
         {
             _chatClient = new ChatClientBuilder(_chatClient).UseFunctionInvocation().Build();
         }
 
-        _promptingService = new PromptingService(_chatClient, _mcpTools);
+        _promptingService = new PromptingService(_chatClient, _tools);
         _initialized = true;
     }
 
@@ -108,13 +115,48 @@ public class ChatService
         return _elicitationCoordinator.HandleAsync(requestParams, token);
     }
 
-    private IClientTransport CreateMcpTransport(IHttpClientFactory clientFactory, string accessToken)
+    private async Task<IClientTransport> CreateMcpTransport(IHttpClientFactory clientFactory)
     {
+        var clientName = "Unsecure Client";
         var httpClient = clientFactory.CreateClient();
-        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-        var httpMcpServerUrl = _configuration["HttpMcpServerUrl"] ?? throw new ArgumentNullException("Configuration missing for HttpMcpServerUrl");
-        return new SseClientTransport(new() { Endpoint = new Uri(httpMcpServerUrl), Name = "Secure Client" }, httpClient);
+
+        if (_functionCallingMode == FunctionCallingMode.McpSecure)
+        {
+            clientName = "Secure Client";
+            var accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync([_configuration["McpScope"]!]);
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        }
+
+        var httpMcpServer = _configuration["HttpMcpServerUrl"] ?? throw new ArgumentNullException("Configuration missing for HttpMcpServerUrl");
+        var transport = new SseClientTransport(new()
+        {
+            Endpoint = new Uri(httpMcpServer!),
+            Name = clientName,
+        }, httpClient);
+
+        return transport;
     }
+
+    private IList<AIFunction> GetLocalTools() => [
+        AIFunctionFactory.Create(
+             () => DateTime.UtcNow.ToString("o"),
+             "GetCurrentDateTime",
+             "Returns the current date and time in ISO 8601 format."),
+        AIFunctionFactory.Create(
+             ([Description("The date to generate random number from")] DateTime? datetime = null) => {
+                if (datetime == null)
+                {
+                    datetime = DateTime.Now;
+                }
+
+                var min = (int)datetime.Value.Ticks % 100;
+                var max = min + 1_000;
+
+                return Random.Shared.Next(min, max);
+            },
+             "GetRandomNumberFromDateTime",
+             "Generates a random number based on a date.")
+    ];
 
     private PromptingService Handler => _promptingService ?? throw new InvalidOperationException("Service not initialized");
 
